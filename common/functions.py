f0aee6f6af18112306a02aebcd0d69916b7c892c
unknown <dotuananh211089@gmail.com>
2022-02-19 17:39:55 +0700

    commit code

--- common/functions.py
+++ common/functions.py
@@ -7,8 +7,8 @@ Created on Sat Feb 19 13:27:52 2022
 
 import sys, os, io
 from dotenv import load_dotenv
-load_dotenv(sys.path.append(os.getenv('/root/airflow/dags/')))
-sys.path.append(os.getenv('/root/airflow/dags/'))
+# load_dotenv(sys.path.append(os.getenv('/root/airflow/dags/')))
+# sys.path.append(os.getenv('/root/airflow/dags/'))
 # from packages.import_packages import *
 import pandas as pd
 from google.cloud import bigquery, storage
