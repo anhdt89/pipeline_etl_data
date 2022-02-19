5dc3d9a656a585d2bdda0d44f6dea69c56947bb2
unknown <dotuananh211089@gmail.com>
2022-02-19 17:27:32 +0700

    commit code

--- packages/import_packages.py
+++ packages/import_packages.py
@@ -4,10 +4,6 @@ Created on Sat Feb 19 13:26:26 2022
 
 @author: Admin
 """
-# import sys, os, io
-# from dotenv import load_dotenv
-# load_dotenv(sys.path.append(os.getenv('/root/airflow/dags/pipeline_etl_data/')))
-# sys.path.append(os.getenv('/root/airflow/dags/pipeline_etl_data/'))
 import pandas as pd
 from google.cloud import bigquery, storage
 from google.oauth2 import service_account
